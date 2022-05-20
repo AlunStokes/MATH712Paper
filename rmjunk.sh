cc7186a95dbb394a5c9617cbcdfbc99db4d98755
Alun Stokes <alunsto99@gmail.com>
2022-05-19 22:10:31 -0400

    U know
    Signed-off-by: Alun Stokes <alunsto99@gmail.com>

--- rmjunk.sh
+++ rmjunk.sh
@@ -1,12 +1,25 @@
-#!/usr/bin/zsh
+#!/bin/sh
 
-rm *.aux
-rm *.synctex.tz
-rm *.thm
-rm *.toc
-rm *.blg
-rm *.idx
-rm *.ind
-rm *.bbl
-rm *.ilg
-rm *.log
+sleep 0.5;
+
+rm *.synctex.gz || echo "Bad synctex";
+rm *.aux || echo "Bad aux";
+rm *.thm || echo "Bad thm";
+rm *.toc || echo "Bad toc";
+rm *.blg || echo "Bad blg";
+rm *.idx || echo "Bad idx";
+rm *.ind || echo "Bad ind";
+rm *.bbl || echo "Bad bbl";
+rm *.ilg || echo "Bad ilg";
+rm *.log || echo "Bad log";
+
+# rm ./output/*.synctex.gz || echo "";
+# rm ./output/*.aux || echo "";
+# rm ./output/*.thm || echo "";
+# rm ./output/*.toc || echo "";
+# rm ./output/*.blg || echo "";
+# rm ./output/*.idx || echo "";
+# rm ./output/*.ind || echo "";
+# rm ./output/*.bbl || echo "";
+# rm ./output/*.ilg || echo "";
+# rm ./output/*.log || echo "";
